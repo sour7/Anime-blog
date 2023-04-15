126789440d018e310a6a4fe1bea4fffcd53850ff
Sourabh Kumar <hellosourabh040@gmail.com>
2023-04-15 22:03:43 +0530

    "commit"

---
+++ server/controllers/userController.js
@@ -0,0 +1,92 @@
+const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+//create user register user
+exports.registerController = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    //validation
+    if (!username || !email || !password) {
+      return res.status(400).send({
+        success: false,
+        message: "Please Fill all fields",
+      });
+    }
+    //exisiting user
+    const exisitingUser = await userModel.findOne({ email });
+    if (exisitingUser) {
+      return res.status(401).send({
+        success: false,
+        message: "user already exisits",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //save new user
+    const user = new userModel({ username, email, password: hashedPassword });
+    await user.save();
+    return res.status(201).send({
+      success: true,
+      message: "New User Created",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Error In Register callback",
+      success: false,
+      error,
+    });
+  }
+};
+
+//login
+exports.loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(401).send({
+        success: false,
+        message: "Please provide email or password",
+      });
+    }
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(200).send({
+        success: false,
+        message: "email is not registerd",
+      });
+    }
+    //password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send({
+        success: false,
+        message: "Invlid username or password",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_TOKEN, {
+      expiresIn: "24h",
+    });
+
+    return res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        success: true,
+        messgae: "login successfully",
+        user,
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error In Login Callcback",
+      error,
+    });
+  }
+};
